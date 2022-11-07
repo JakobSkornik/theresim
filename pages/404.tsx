224ae5855a46b6722dc9f4e79ce853e4c3c6eb60
skornikjakob <skornikjakobs@gmail.com>
2022-11-07 23:48:00 +0100

    Added a custom 404 page

---
+++ pages/404.tsx
@@ -0,0 +1,8 @@
+import P5Container from '../components/P5Container'
+import scene from '../modules/p5/404/scene'
+
+const Custom404 = () => {
+  return <P5Container title={'404'} mediapipe={false} scene={scene} icon="controller.svg" />
+}
+
+export default Custom404
